c52584fbe0c72581fc2713421ecd159c960485b6
Stefan Ziegler <edi.gonzales@gmail.com>
2025-06-11 19:09:07 +0200

    wip

--- src/main/java/ch/so/agi/meta/ResourceRuntimeHints.java
+++ src/main/java/ch/so/agi/meta/ResourceRuntimeHints.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import gg.jte.generated.precompiled.JteindexGenerated;
 import gg.jte.generated.precompiled.JteproductGenerated;
+import gg.jte.generated.precompiled.JteproductdetailGenerated;
 import gg.jte.generated.precompiled.JtesearchresultsGenerated;
 
 @Component
@@ -21,7 +22,7 @@ public class ResourceRuntimeHints implements RuntimeHintsRegistrar {
             .registerType(JteindexGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
             .registerType(JteproductGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
             .registerType(JtesearchresultsGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
-            .registerType(JtesearchresultsGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS);
+            .registerType(JteproductdetailGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS);
         
     }
 
