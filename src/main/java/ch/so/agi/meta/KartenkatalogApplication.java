3e6e37a499065dfe20489ed5238f9e8b5106ed94
Stefan Ziegler <edi.gonzales@gmail.com>
2025-12-15 17:11:10 +0100

    try to fix native image

--- src/main/java/ch/so/agi/meta/KartenkatalogApplication.java
+++ src/main/java/ch/so/agi/meta/KartenkatalogApplication.java
@@ -1,5 +1,6 @@
 package ch.so.agi.meta;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import ch.so.agi.meta.ResourceRuntimeHints;
 @ImportRuntimeHints(ResourceRuntimeHints.class)
 @Configuration
 @SpringBootApplication
+@RegisterReflectionForBinding({ ch.so.agi.meta.Thema.class, ch.so.agi.meta.Ebene.class })
 public class KartenkatalogApplication {
 
     public static void main(String[] args) {
